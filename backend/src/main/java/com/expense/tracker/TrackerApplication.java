aa9cf1cb3465058e43f640fcc33bae481b4d9869
anhedonic09 <akashbisht0917@gmail.com>
2025-02-26 00:07:56 +0530

    simple signup and login APIs

--- backend/src/main/java/com/expense/tracker/TrackerApplication.java
+++ backend/src/main/java/com/expense/tracker/TrackerApplication.java
@@ -3,8 +3,6 @@ package com.expense.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.security.SecureRandom;
-
 @SpringBootApplication
 public class TrackerApplication {
 
